df39ac4736866d75c40936652d87228a1ce2b794
Raymond Cox <raymond@raymondjcox.com>
2022-05-08 10:43:54 -0400

    this don't work yet, but it looks cool

--- src/app.tsx
+++ src/app.tsx
@@ -14,14 +14,28 @@ const render = init([
   eventListenersModule,
 ]);
 
+function useState<T>(initialState: T): [T, (newState: T) => void] {
+  let state = initialState;
+  let setState = (newState: T) => {
+    state = newState;
+  };
+
+  return [state, setState];
+}
+
 function MyComponent({ name }) {
   const time = new Date().getTime();
+  const [goodbye, setGoodbye] = useState(false);
 
   return (
     <div>
-      Hello {name} the time is {time}
+      <button on={{ click: () => setGoodbye(true) }}>Goodbye</button>
+      <div>
+        {goodbye ? "Goodbye" : "Hello"} {name} the time is {time}
+      </div>
     </div>
   );
 }
+
 const container = document.getElementById("container");
 render(container, <MyComponent name="RJ" />);
